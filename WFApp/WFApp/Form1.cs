f5b9a856bdaafbbd69c783d152355533a358c8e8
mactepwar <mactepwar@outlook.com>
2019-05-23 10:02:39 +0300

    init

---
+++ WFApp/WFApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp;
+        Graphics gr;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            byte[] data = new byte[1024];
+            int sent;
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5678);
+
+            Socket server = new Socket(AddressFamily.InterNetwork,
+                            SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ipep);
+            }
+            catch (SocketException ee)
+            {
+                Console.WriteLine("Unable to connect to server.");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+            
+            MemoryStream ms = new MemoryStream();
+
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            // Читаю до конца
+            byte[] bmpBytes = ms.GetBuffer();
+            bmp.Dispose();
+            ms.Close();
+
+            sent = SendVarData(server, bmpBytes);
+
+            Console.WriteLine("Disconnecting from server...");
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+            Console.ReadLine();
+        }
+
+        private static int SendVarData(Socket s, byte[] data)
+        {
+            int total = 0;
+            int size = data.Length;
+            int dataleft = size;
+            int sent;
+
+            byte[] datasize = new byte[4];
+            datasize = BitConverter.GetBytes(size);
+            sent = s.Send(datasize);
+
+            while (total < size)
+            {
+                sent = s.Send(data, total, dataleft, SocketFlags.None);
+                total += sent;
+                dataleft -= sent;
+            }
+            return total;
+        }
+    }
+}
---
+++ WPAppServTCP/WPAppServTCP/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WPAppServTCP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static byte[] ReceiveVarData(Socket s)
+        {
+            int total = 0;
+            int recv;
+            byte[] datasize = new byte[4];
+
+            recv = s.Receive(datasize, 0, 4, 0);
+            int size = BitConverter.ToInt32(datasize, 0);
+            int dataleft = size;
+            byte[] data = new byte[size];
+
+
+            while (total < size)
+            {
+                recv = s.Receive(data, total, dataleft, 0);
+                if (recv == 0)
+                {
+                    break;
+                }
+                total += recv;
+                dataleft -= recv;
+            }
+            return data;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Thread(() => {
+                Console.WriteLine("Server is starting...");
+                byte[] data = new byte[1024];
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5678);
+
+                Socket newsock = new Socket(AddressFamily.InterNetwork,
+                                SocketType.Stream, ProtocolType.Tcp);
+
+                newsock.Bind(ipep);
+                newsock.Listen(10);
+                Console.WriteLine("Waiting for a client...");
+
+                Socket client = newsock.Accept();
+                IPEndPoint newclient = (IPEndPoint)client.RemoteEndPoint;
+                Console.WriteLine("Connected with {0} at port {1}",
+                                newclient.Address, newclient.Port);
+
+                while (true)
+                {
+                    data = ReceiveVarData(client);
+                    MemoryStream ms = new MemoryStream(data);
+                    try
+                    {
+                        Image bmp = Image.FromStream(ms);
+                        pictureBox1.Image = bmp;
+                    }
+                    catch (ArgumentException ee)
+                    {
+                        Console.WriteLine("something broke");
+                    }
+
+
+                    if (data.Length == 0)
+                        newsock.Listen(10);
+                }
+                client.Close();
+                newsock.Close();
+            }).Start();
+            
+        }
+    }
+}
